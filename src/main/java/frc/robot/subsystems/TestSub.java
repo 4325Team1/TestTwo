ed699549d4809c5f5a24a6f456de9bbb6b7b8266
4325Mentor <tgreen.cass@gmail.com>
2025-02-21 16:58:05 -0500

    Tim's update

--- src/main/java/frc/robot/subsystems/TestSub.java
+++ src/main/java/frc/robot/subsystems/TestSub.java
@@ -3,14 +3,17 @@
 // the WPILib BSD license file in the root directory of this project.
 package frc.robot.subsystems;
 
-import com.revrobotics.AbsoluteEncoder;
 import com.revrobotics.RelativeEncoder;
-import com.revrobotics.servohub.ServoHub.ResetMode;
 import com.revrobotics.spark.SparkClosedLoopController;
 import com.revrobotics.spark.SparkMax;
 import com.revrobotics.spark.SparkBase.PersistMode;
+import com.revrobotics.spark.SparkBase.ResetMode;
+import com.revrobotics.spark.config.ClosedLoopConfig;
 import com.revrobotics.spark.config.SparkMaxConfig;
 import com.revrobotics.spark.config.ClosedLoopConfig.FeedbackSensor;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.SparkBase.ControlType;
+import com.revrobotics.spark.ClosedLoopSlot;
 
 import edu.wpi.first.networktables.GenericEntry;
 import edu.wpi.first.wpilibj.Joystick;
@@ -25,7 +28,6 @@ public class TestSub extends SubsystemBase {
   public SparkMax motor;
   private SparkClosedLoopController pidController;
   public RelativeEncoder encoder;
-  private Joystick joystick;
   
   public double kP = 0;
   public double kI = 0;
@@ -38,6 +40,8 @@ public class TestSub extends SubsystemBase {
   public double maxVelocity = 0;
   public double actualPosition = 0;
   public double setpointGlobal = 0;
+  public double allowedClosedLoopError = 0;
+  public boolean doUpdatePID = true;
 
   private GenericEntry pEntry;
   private GenericEntry iEntry;
@@ -46,14 +50,19 @@ public class TestSub extends SubsystemBase {
   private GenericEntry ffEntry;
   private GenericEntry maxAccelEntry;
   private GenericEntry maxVelEntry;
+  private GenericEntry closedLoopErrorEntry;
   private GenericEntry setPointEntry;
 
   public double maxRPM = 5700;
   public SparkMaxConfig motorConfig = new SparkMaxConfig();
 
 
+
   public TestSub() 
   {
+
+
+
   // assign entrys
     ShuffleboardTab pidPosition = Shuffleboard.getTab("Pid Position");
     pEntry = pidPosition.add("kP",0).getEntry();
@@ -63,6 +72,7 @@ public class TestSub extends SubsystemBase {
     ffEntry = pidPosition.add("kFF",0).getEntry();
     maxAccelEntry = pidPosition.add("kAccel",0).getEntry();
     maxVelEntry = pidPosition.add("kVel",0).getEntry();
+    closedLoopErrorEntry = pidPosition.add("Allowed Closed Loop Error", 0).getEntry();
     setPointEntry = pidPosition.add("kSetPoint",0).getEntry();
     
   // Create motors
@@ -76,101 +86,133 @@ public class TestSub extends SubsystemBase {
     encoder = motor.getEncoder();
     
 
-    motorConfig.closedLoop.feedbackSensor(FeedbackSensor.kPrimaryEncoder).pidf
-    (
-      kP,
-      kI,
-      kD,
-      kFF
-    );
-    motorConfig.closedLoop.iZone(kIz);
-    motorConfig.closedLoop.maxMotion.maxAcceleration(maxVelocity);
-    motorConfig.closedLoop.maxMotion.maxVelocity(maxVelocity);
+    //motorConfig.closedLoop.feedbackSensor(FeedbackSensor.kPrimaryEncoder).pidf
+    //(
+    //  kP,
+    //  kI,
+    //  kD,
+    //  kFF
+    //);
+    //motorConfig.closedLoop.iZone(kIz);
+    //motorConfig.closedLoop.maxMotion.maxAcceleration(maxVelocity);
+    //motorConfig.closedLoop.maxMotion.maxVelocity(maxVelocity);
   
     motorConfig.smartCurrentLimit(80, 40);
-    motor.configure(motorConfig, com.revrobotics.spark.SparkBase.ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+    //motor.configure(motorConfig, com.revrobotics.spark.SparkBase.ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
     
 
   }
-  
-   public void updatePIDConstants() 
-   {
 
-    double p = pEntry.getDouble(0);
-    double i = iEntry.getDouble(0);
-    double d = dEntry.getDouble(0);
-    double iz = iZEntry.getDouble(0);
-    double ff = ffEntry.getDouble(0);
-    double maxAccel = maxAccelEntry.getDouble(0);
-    double maxVel = maxVelEntry.getDouble(0);
-    double setPoint = setPointEntry.getDouble(0);
+  public void updatePIDConstants() 
+  {
 
-    if(p != kP)
-    { motorConfig.closedLoop.p(kP);
-            kP = p;
+    double p = pEntry.getDouble(kP);
+    double i = iEntry.getDouble(kI);
+    double d = dEntry.getDouble(kD);
+    double iz = iZEntry.getDouble(kIz);
+    double ff = ffEntry.getDouble(kFF);
+    double maxAccel = maxAccelEntry.getDouble(maxAcceleration);
+    double maxVel = maxVelEntry.getDouble(maxVelocity);
+    double closedLoopError = closedLoopErrorEntry.getDouble(allowedClosedLoopError);
+    double setPoint = setPointEntry.getDouble(setpointGlobal);
+
+    if(p != kP){ 
+      //motorConfig.closedLoop.p(kP);
+      kP = p;
+      doUpdatePID = true;
     }
           
     if (i != kI) {
-      motorConfig.closedLoop.i(kI);
-        kI = i;
+      //motorConfig.closedLoop.i(kI);
+      kI = i;
+      doUpdatePID = true;
     }
     if (d != kD) {
-      motorConfig.closedLoop.d(kD);
-        kD = d;
+      //motorConfig.closedLoop.d(kD);
+      kD = d;
+      doUpdatePID = true;
     }
     if (iz != kIz) {
-      motorConfig.closedLoop.iZone(kIz);
-        kIz = iz;
+      //motorConfig.closedLoop.iZone(kIz);
+      kIz = iz;
+      doUpdatePID = true;
     }
     if (ff != kFF) {
-      motorConfig.closedLoop.velocityFF(ff);
-        kFF = ff;
+      //motorConfig.closedLoop.velocityFF(ff);
+      kFF = ff;
+      doUpdatePID = true;
     }
-    if (maxAccel != maxAcceleration)
-    {
-      motorConfig.closedLoop.maxMotion.maxAcceleration(maxAcceleration);
+    if (maxAccel != maxAcceleration){
+      //motorConfig.closedLoop.maxMotion.maxAcceleration(maxAcceleration);
       maxAcceleration = maxAccel;
+      doUpdatePID = true;
     }
 
-    if (maxVel != maxVelocity)
-    {
-      motorConfig.closedLoop.maxMotion.maxAcceleration(maxVelocity);
-        maxVelocity = maxVel;
+    if (maxVel != maxVelocity){
+      //motorConfig.closedLoop.maxMotion.maxAcceleration(maxVelocity);
+      maxVelocity = maxVel;
+      doUpdatePID = true;
     }
-    if (setPoint != setpointGlobal)
-      {
+
+    if(closedLoopError != allowedClosedLoopError){
+      allowedClosedLoopError = closedLoopError;
+      doUpdatePID = true;
+    }
+
+    if (setPoint != setpointGlobal){
       //  pidController.setReference(setpointGlobal, SparkMax.ControlType.kPosition);
         setpointGlobal = setPoint;
-      }
     }
-      
-  
-      public void updateMotorControl() {
-          // Setpoint calculation based on joystick input
-          updatePIDConstants();
-          getPosition();
-          // Set PID controller setpoint for velocity control
-          pidController.setReference(setpointGlobal, SparkMax.ControlType.kMAXMotionPositionControl);
-  
-          // Push setpoint and current velocity to SmartDashboard
-        /////  SmartDashboard.putNumber("SetPoint", setpoint);
-        /////  SmartDashboard.putNumber("ProcessVariable", encoder.getPosition());
-      }
-      public void getPosition()
-      {
-       actualPosition = encoder.getPosition();
-      }
-      public void setSpeed()
-      {
-        motor.set(0.5);
-      }
-      public void stopSpeed()
-      {
-        motor.set(0.);
-      }
-      public void setZero()
-      {
-        encoder.setPosition(0);
-      }
+
+    if(doUpdatePID){
+      motorConfig.closedLoop
+      .feedbackSensor(FeedbackSensor.kPrimaryEncoder)
+      // Set PID values for position control. We don't need to pass a closed
+      // loop slot, as it will default to slot 0.
+      .pidf(p, i, d, ff)
+      .iZone(iz)
+      .outputRange(-1, 1);
+
+    motorConfig.closedLoop.maxMotion
+      // Set MAXMotion parameters for position control. We don't need to pass
+      // a closed loop slot, as it will default to slot 0.
+      .maxVelocity(maxVel)
+      .maxAcceleration(maxAccel)
+      .allowedClosedLoopError(closedLoopError);
+
+      motor.configure(motorConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+
+      doUpdatePID = false;
+    }
+
+  }
+    
+    public void updateMotorControl() {
+        // Setpoint calculation based on joystick input
+        updatePIDConstants();
+        getPosition();
+        // Set PID controller setpoint for velocity control
+        pidController.setReference(setpointGlobal, SparkMax.ControlType.kMAXMotionPositionControl);
+
+        // Push setpoint and current velocity to SmartDashboard
+      /////  SmartDashboard.putNumber("SetPoint", setpoint);
+      /////  SmartDashboard.putNumber("ProcessVariable", encoder.getPosition());
+    }
+    public void getPosition()
+    {
+      actualPosition = encoder.getPosition();
+    }
+    public void setSpeed()
+    {
+      motor.set(0.5);
+    }
+    public void stopSpeed()
+    {
+      motor.set(0.);
+    }
+    public void setZero()
+    {
+      encoder.setPosition(0);
+    }
         
   }
